95416b4a93bce13397df70361b4a48bb036f0e74
FonRiego <fonriego@gmail.com>
2018-09-24 13:54:01 +0200

    Models and Seeds Done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router  = express.Router();
+const Story = require('../models/Story')
+const User = require('../models/User')
+const Collab = require('../models/Collab')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/stories', (req, res, next) => {
+  Story.find()
+  .populate('collaborations')
+  .then(stories => {
+    res.render('stories', {stories})
+  })
+})
 
 module.exports = router;
