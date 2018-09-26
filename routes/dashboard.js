47688141f707e88c78518bbcc49f250735f3dd79
FonRiego <fonriego@gmail.com>
2018-09-26 15:33:24 +0200

    view more stories to continue implemented

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,6 +8,7 @@ const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 router.get('/dashboard', ensureLoggedIn('/login'), (req, res, next) => {
   let user = req.user._id;
   Story.find({"open": true, "users": {$ne: user._id}})
+  .limit(4)
   .populate({ 
     path: 'collaborations',
     populate: {
@@ -18,7 +19,26 @@ router.get('/dashboard', ensureLoggedIn('/login'), (req, res, next) => {
   .then(stories => {
     res.render('dashboard', {stories})
   })
+  .catch(e => console.log(e))
 })
+
+
+router.get('/dashboard/openstories', ensureLoggedIn('/login'), (req, res, next) => {
+    let user = req.user._id;
+    Story.find({"open": true, "users": {$ne: user._id}})
+    .populate({ 
+      path: 'collaborations',
+      populate: {
+        path: 'user'
+      } 
+    })
+    .populate('users')
+    .then(stories => {
+      res.render('stories/open', {stories})
+    })
+    .catch(e => console.log(e))
+  })
+
 
 
 module.exports = router;
