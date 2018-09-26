a26485f6c9a0ad85b9acf953c26ca9b34b2ee7b1
Matt Ch. Rocha <mattchrocha@gmail.com>
2018-09-26 22:04:51 +0200

    view of  new and continue  stories update

--- public/javascripts/getGif.js
+++ public/javascripts/getGif.js
@@ -11,7 +11,7 @@ $(document).ready( ()=> {
 
 const getRandomGif = () => {
   axios
-  .get("http://api.giphy.com/v1/gifs/random?api_key=6XPOIbXZf3DCR3aJ0gzBBP8zEyN8mS3K")
+  .get("http://api.giphy.com/v1/gifs/random?api_key=ddaUoP3GezvuVlDAysZWrxIr6W85UxTd")
   .then( giphy => {
     let gifUrl = giphy.data.data.image_original_url;
     $('#giphy-image-randomizer img').prop("src", gifUrl);
