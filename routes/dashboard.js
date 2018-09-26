29371b53d09d221407cc1b2e34b2043e689e5aaf
FonRiego <fonriego@gmail.com>
2018-09-26 18:02:34 +0200

    dashboard updated

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,7 +7,9 @@ const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 router.get('/dashboard', ensureLoggedIn('/login'), (req, res, next) => {
   let user = req.user._id;
-  Story.find({"open": true, "users": {$ne: user._id}})
+  console.log(user)
+
+  const prom1 = Story.find({"open": true, "users": {$ne: user._id}})
   .limit(4)
   .populate({ 
     path: 'collaborations',
@@ -16,8 +18,20 @@ router.get('/dashboard', ensureLoggedIn('/login'), (req, res, next) => {
     } 
   })
   .populate('users')
-  .then(stories => {
-    res.render('dashboard', {stories})
+
+  const prom2 = Story.find({"open": false, "users": {$in: user._id}})
+  .limit(4)
+  .populate({ 
+    path: 'collaborations',
+    populate: {
+      path: 'user'
+    } 
+  })
+  .populate('users')
+
+  Promise.all([prom1, prom2])
+  .then(([openstories, mystories]) => {
+    res.render('dashboard', {openstories, mystories})
   })
   .catch(e => console.log(e))
 })
@@ -34,7 +48,23 @@ router.get('/dashboard/openstories', ensureLoggedIn('/login'), (req, res, next) 
     })
     .populate('users')
     .then(stories => {
-      res.render('stories/open', {stories})
+      res.render('stories/opened-stories', {stories})
+    })
+    .catch(e => console.log(e))
+  })
+
+  router.get('/dashboard/mystories', ensureLoggedIn('/login'), (req, res, next) => {
+    let user = req.user._id;
+    Story.find({"open": false, "users": {$in: user._id}})
+    .populate({ 
+      path: 'collaborations',
+      populate: {
+        path: 'user'
+      } 
+    })
+    .populate('users')
+    .then(stories => {
+      res.render('stories/my-completed-stories', {stories})
     })
     .catch(e => console.log(e))
   })
