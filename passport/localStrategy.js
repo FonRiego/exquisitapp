62133f6e4faede0d861b719e0a56236d9d163320
FonRiego <fonriego@gmail.com>
2018-09-27 20:55:42 +0200

    implementing Facebook

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,6 +1,5 @@
 const passport      = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-// const FacebookStrategy = require('passport-facebook').Strategy;
 const User          = require('../models/User');
 const bcrypt        = require('bcrypt');
 
@@ -26,16 +25,3 @@ passport.use(new LocalStrategy({
     .catch(err => done(err));
   }
 ));
-
-// passport.use(new FacebookStrategy({
-//   clientID: FACEBOOK_APP_ID,
-//   clientSecret: FACEBOOK_APP_SECRET,
-//   callbackURL: "http://www.example.com/auth/facebook/callback"
-// },
-// function(accessToken, refreshToken, profile, done) {
-//   User.findOrCreate(..., function(err, user) {
-//     if (err) { return done(err); }
-//     done(null, user);
-//   });
-// }
-// ));
