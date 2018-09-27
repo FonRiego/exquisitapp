2bf131e1cefd6d7e460f88d1f6f1f96edec5e173
Matt Ch. Rocha <mattchrocha@gmail.com>
2018-09-27 18:20:52 +0200

    on ma way

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ hbs.registerHelper('ifUndefined', (value, options) => {
   
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Exquisit.app – Creador de memes colaborativos';
 
 
 // Enable authentication using session + passport
