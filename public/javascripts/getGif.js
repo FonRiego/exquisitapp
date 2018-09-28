c375e52880e65598cab7a65ab18ac9d1b8bbd74f
FonRiego <fonriego@gmail.com>
2018-09-28 14:11:42 +0200

    new giphy API KEY

--- public/javascripts/getGif.js
+++ public/javascripts/getGif.js
@@ -11,7 +11,7 @@ $(document).ready( ()=> {
 
 const getRandomGif = () => {
   axios
-  .get("https://api.giphy.com/v1/gifs/random?api_key=pICam2pCKXI5lGSFpXFvWsNEvZuiARPh")
+  .get("https://api.giphy.com/v1/gifs/random?api_key=lDIdvYhkGMTpAXEjRsXHmekRz9VVwDzX")
   .then( giphy => {
     let gifUrl = giphy.data.data.image_original_url;
     $('#giphy-image-randomizer img').prop("src", gifUrl);
