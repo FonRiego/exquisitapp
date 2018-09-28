35a10d5b558676c2a80181f8cc1b7cb379479fbd
FonRiego <fonriego@gmail.com>
2018-09-28 12:09:16 +0200

    facebook done

--- passport/facebook.js
+++ passport/facebook.js
@@ -7,8 +7,8 @@ const findOrCreate = require('mongoose-find-or-create')
 passport.use(new facebook({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/callback"
-  // callbackURL: "https://exquisit-app.herokuapp.com/auth/facebook/callback"
+  // callbackURL: "http://localhost:3000/auth/facebook/callback"
+  callbackURL: "https://exquisit-app.herokuapp.com/auth/facebook/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   User.findOrCreate({ username: profile.displayName, facebookId: profile.id }, function (err, user) {
