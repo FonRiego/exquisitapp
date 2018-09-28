ad462be5004e9658b2b3f0ff93e7873c9121c476
FonRiego <fonriego@gmail.com>
2018-09-28 13:29:21 +0200

    giphy apikey changed

--- public/javascripts/getGif.js
+++ public/javascripts/getGif.js
@@ -11,7 +11,7 @@ $(document).ready( ()=> {
 
 const getRandomGif = () => {
   axios
-  .get("https://api.giphy.com/v1/gifs/random?api_key=ddaUoP3GezvuVlDAysZWrxIr6W85UxTd")
+  .get("https://api.giphy.com/v1/gifs/random?api_key=pICam2pCKXI5lGSFpXFvWsNEvZuiARPh")
   .then( giphy => {
     let gifUrl = giphy.data.data.image_original_url;
     $('#giphy-image-randomizer img').prop("src", gifUrl);
